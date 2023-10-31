85c3d17253415c7bd2c5c2bc9646bcb089fbbbe1
michalnawrockidj <92578842+michalnawrockidj@users.noreply.github.com>
2023-10-31 09:12:06 +0000

    MAP-373 adding endpoint (#17)
    * MAP-373 adding endpoint
    
    * Code review changes

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercellallocationapi/model/dto/MoveToCellResponse.kt
@@ -0,0 +1,11 @@
+package uk.gov.justice.digital.hmpps.prisonercellallocationapi.model.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class MoveToCellResponse(
+  @Schema(
+    description = "Move to cell record id",
+    example = "1234",
+  )
+  var id: Long,
+)
